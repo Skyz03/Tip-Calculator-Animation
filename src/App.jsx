87fb8b4f4773e37ced87bc0c932ed3675ed0de36
Asia Ashraf <asiaashraf7272@gmail.com>
2025-11-02 10:34:10 +0500

    add basic setup of tipcalculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import TipCalculator
+  from "./components/TipCalculator"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Tailwind is working! 
-    </h1>
+    <TipCalculator />
   )
 }
---
+++ src/components/TipCalculator.jsx
@@ -0,0 +1,39 @@
+import { useForm } from "react-hook-form";
+
+export default function TipCalculator() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Bill Amount:</label>
+      <input
+        type="number"
+        placeholder="Enter bill"
+        {...register("bill", { required: true, min: 1 })}
+      />
+      {errors.bill && <span>Please enter a valid bill</span>}
+
+      <label>Select Tip %:</label>
+      <input
+        type="number"
+        placeholder="Tip %"
+        {...register("tip", { required: true })}
+      />
+      {errors.tip && <span>Select tip percentage</span>}
+
+      <label>Number of People:</label>
+      <input
+        type="number"
+        placeholder="People count"
+        {...register("people", { required: true, min: 1 })}
+      />
+      {errors.people && <span>Enter number of people</span>}
+
+      <button type="submit">Calculate</button>
+    </form>
+  );
+}
