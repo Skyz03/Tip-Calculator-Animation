d57a65ce50983b73d4cdf82371b7d88fcc26327f
Asia Ashraf <asiaashraf7272@gmail.com>
2025-11-05 13:53:59 +0500

    apply styling in number of people section

--- src/components/TipCalculator.jsx
+++ src/components/TipCalculator.jsx
@@ -26,7 +26,8 @@ export default function TipCalculator() {
     <div className="flex flex-col md:flex-row gap-6 w-full max-w-[400px] sm:max-w-[500px] md:max-w-[600px]  bg-white text-black rounded-2xl p-4 ">
 
       <form onSubmit={handleSubmit(onSubmit)} className="w-full md:w-1/2">
-        <div className="min-w-full  mb-8">
+        {/* Bill seciton*/}
+        <section className="min-w-full  mb-8">
           <label className="text-sm text-gray-600 font-semibold">Bill</label>
           <div className="relative mt-1">
             <img src={dollarIcon} alt="doller_Icon"
@@ -41,10 +42,10 @@ export default function TipCalculator() {
           </div>
 
           {errors.bill && <span className="text-red-500 text-xs">Enter valid bill</span>}
-        </div>
+        </section>
 
-        {/* Tip Select Buttons */}
-        <div className="mb-8">
+        {/* Tip Select Buttons section */}
+        <section className="mb-8">
           <label className="block text-sm font-semibold">Select Tip %</label>
           <div className="grid grid-cols-3 gap-3 my-3">
             {tipOptions.map((tip) => (
@@ -72,22 +73,33 @@ export default function TipCalculator() {
             />
 
           </div>
-        </div>
+        </section>
 
+        {/* Number of People section */}
+        <section className="mb-8">
+          <label>Number of People:</label>
+          <div className="relative">
+            <img src={iconPerson} alt="personIcon"
+              className="absolute left-4 top-1/2 -translate-y-1/2 w-3" />
+            <input
+              type="number"
+              className="w-full bg-[hsl(189,41%,97%)] p-3 pl-10 pr-3 text-right text-[hsl(183,100%,15%)] rounded-sm font-bold outline-none focus:ring-2 focus:ring-[hsl(172,67%,45%)]"
+              placeholder="People count"
+              {...register("people", { required: true, min: 1 })}
+            />
+          </div>
+          {errors.people && <span className="text-red-500 text-xs">Enter number of people</span>}
+        </section>
 
-        <label>Number of People:</label>
-        <img src={iconPerson} alt="personIcon"
-          className="absolute" />
-        <input
-          type="number"
-          className="w-full relative p-2 text-right  text-[rgb(0,73,77)]"
-          placeholder="People count"
-          {...register("people", { required: true, min: 1 })}
-        />
-        {errors.people && <span>Enter number of people</span>}
 
         <button type="submit">Calculate</button>
       </form>
+
+
+
+
+
+
       {/* result of tip calculator */}
       <div className="w-full md:w-1/2 bg-[hsl(183,100%,15%)] text-white p-4 rounded-xl">
         <h1>Here resulte should be display</h1>
