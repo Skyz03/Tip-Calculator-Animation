343f7dd83e01a0be53b0942d6419a65b0ec2f6fe
Asia Ashraf <asiaashraf7272@gmail.com>
2025-11-07 17:03:53 +0500

    logo component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import TipCalculator
-  from "./components/TipCalculator"
+import TipCalculator from "./components/TipCalculator"
+import Logo from "./components/Logo"
 
 
 export default function App() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-[hsl(185,41%,84%)]
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-[hsl(185,41%,84%)]
  text-white p-4">
+      <Logo />
       <TipCalculator />
     </div>
   )
 }
 
-// - Green 900: hsl(183, 100%, 15%)
-// - Grey 500: hsl(186, 14%, 43%)
-// - Grey 400: hsl(184, 14%, 56%)
-// - Grey 200: hsl(185, 41%, 84%)
-// - Grey 50: hsl(189, 47%, 97%)
-// - White: hsl(0, 100%, 100%)
